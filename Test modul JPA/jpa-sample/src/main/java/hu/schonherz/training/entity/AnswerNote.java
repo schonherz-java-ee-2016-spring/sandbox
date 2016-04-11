cb7177861a3bc8e50d2007ed9be36b7b9d48170a
Fekete Attila <atkavagyok@gmail.com>
2016-04-11 22:23:41 +0200

    LOB bugfix

--- Test modul JPA/jpa-sample/src/main/java/hu/schonherz/training/entity/AnswerNote.java
+++ Test modul JPA/jpa-sample/src/main/java/hu/schonherz/training/entity/AnswerNote.java
@@ -1,26 +1,25 @@
 package hu.schonherz.training.entity;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Type;
+
 @Entity
 @Table(name = "answer_note")
-public class AnswerNote implements Serializable {
+public class AnswerNote extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
-	@Id
 	@OneToOne
 	@JoinColumn(name = "answer_id", referencedColumnName = "id")
 	private Answer answer;
 
 	@Column(nullable = true)
+	@Type(type = "org.hibernate.type.StringClobType")
 	@Lob
 	private String note;
 
--- Test modul JPA/jpa-sample/src/main/java/hu/schonherz/training/entity/AnswerText.java
+++ Test modul JPA/jpa-sample/src/main/java/hu/schonherz/training/entity/AnswerText.java
@@ -1,25 +1,23 @@
 package hu.schonherz.training.entity;
 
-import java.io.Serializable;
-
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Type;
+
 @Entity
 @Table(name = "answer_text")
-public class AnswerText implements Serializable {
+public class AnswerText extends BaseEntity {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
+
 	@OneToOne
 	@JoinColumn(name = "answer_id", referencedColumnName = "id")
 	private Answer answer;
-	
 
+	@Type(type = "org.hibernate.type.StringClobType")
 	@Lob
 	private String text;
 
@@ -42,6 +40,5 @@ public class AnswerText implements Serializable {
 	public void setAnswer(Answer answer) {
 		this.answer = answer;
 	}
-
 
 }
